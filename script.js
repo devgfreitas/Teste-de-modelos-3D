b471aea10317d1cf6358864e2dc5c0fdc75e5eb3
Giovany Freitas <gvnyfreitas@gmail.com>
2026-06-15 14:08:16 -0300

    Add class to model-viewer elements in clique function
    Added 'class="modelovisual"' to model-viewer elements.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let quadro = document.querySelector('.quadro');
 
 function clique(numberclass) {
     if(numberclass == 1) {
-        quadro.innerHTML = '<model-viewer src="burgerturtlerigged.glb" auto-rotate camera-controls style="width: 1000px; height: 600px;" shadow-intensity="1" exposure="1"></model-viewer>';
+        quadro.innerHTML = '<model-viewer src="burgerturtlerigged.glb" class="modelovisual" auto-rotate camera-controls shadow-intensity="1" exposure="1"></model-viewer>';
     } else if(numberclass == 2) {
-        quadro.innerHTML = '<model-viewer src="clippit.glb" auto-rotate camera-controls style="width: 1000px; height: 600px;" shadow-intensity="1" exposure="1"></model-viewer>';
+        quadro.innerHTML = '<model-viewer src="clippit.glb" class="modelovisual" auto-rotate camera-controls shadow-intensity="1" exposure="1"></model-viewer>';
     } else {
-        quadro.innerHTML = '<model-viewer src="bart_simpson.glb" auto-rotate camera-controls style="width: 1000px; height: 600px;" shadow-intensity="1" exposure="1"></model-viewer>';
+        quadro.innerHTML = '<model-viewer src="bart_simpson.glb" class="modelovisual" auto-rotate camera-controls shadow-intensity="1" exposure="1"></model-viewer>';
     }
 }
